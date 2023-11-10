8f701c191fd04b0052358247f2ca8cdb075f7364
tebemargono <87219104+tebemargono@users.noreply.github.com>
2023-11-10 04:21:45 +0000

    pakai css

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_there():
 	return render_template('home.html')
 
 if __name__ == '__main__':
